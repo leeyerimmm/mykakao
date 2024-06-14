e28628a635bb80bd971927542f07832e2d459c1c
leeyerim <leeyltf20@naver.com>
2024-06-14 16:25:47 +0900

    first commit

---
+++ js/function.js
@@ -0,0 +1,21 @@
+function getTime(){
+  const nowTime = new Date();
+  let hours = nowTime.getHours();
+  let minutes = nowTime.getMinutes();
+
+  if (hours < 10) {
+    hours = '0' + hours;
+}
+if (minutes < 10) {
+    minutes = '0' + minutes;
+}
+const time = `${hours}:${minutes}`;
+document.getElementById('updateTime').textContent = time;
+}
+
+function init(){
+  getTime();
+  setInterval(getTime,1000);
+}
+
+init();
